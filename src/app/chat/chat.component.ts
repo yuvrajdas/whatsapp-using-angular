fcc422e0a421ce4d16b4f8d81135d7a1d1146f1e
Yuvraj Das <yuvraj.m@atai.ai>
2022-12-13 16:02:54 +0530

    layout structure completed

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class ChatComponent implements OnInit {
 
   constructor() { }
+@Input() conversation:any;
 
   ngOnInit(): void {
+    console.log(this.conversation);
   }
-
+  submitMessage(){
+    
+  }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -12,103 +12,291 @@ export class SidebarComponent implements OnInit {
   conversations:any[] =  [{
     message: "60639 Packers Lane",
     name: "Pia",
-    time: "4:08 PM"
+    time: "4:08 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:false},
+      {id:1, body:'Hello world', time:'4:08', me:false},
+      {id:1, body:'Hello world', time:'4:08', me:false},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:false},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "2877 Summerview Circle",
     name: "Constantine",
-    time: "1:34 PM"
+    time: "1:34 PM",
+    messages:[
+      {
+        id: 1,
+        me: false,
+        time: "6:12 AM",
+        body: "Happy Birthday....!"
+      },
+      {id:2, body:'Another message for you', time:'4:08', me:true},
+      {id:3, body:'Hi', time:'4:08', me:true},
+      {id:4, body:'Go to hell', time:'4:08', me:true},
+      {id:5, body:'ok bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "55 Mallory Lane",
     name: "Leta",
-    time: "12:37 PM"
+    time: "12:37 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:false},
+      {id:2, body:'How are you', time:'4:08', me:false},
+      {id:3, body:'Thanks', time:'4:08', me:false},
+      {id:4, body:'Good night', time:'4:08', me:false},
+      {id:5, body:'Bye...!', time:'4:08', me:false},
+     
+    ]
   }, {
     message: "0052 North Avenue",
     name: "Priscilla",
-    time: "6:47 AM"
+    time: "6:47 AM",
+    messages:[
+      {
+        id: 1,
+        me: false,
+        time: "3:57 AM",
+        body: "Computers"
+      },
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "863 Mcbride Place",
     name: "Cyndie",
-    time: "10:11 AM"
+    time: "10:11 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "0 High Crossing Drive",
     name: "Tatiana",
-    time: "12:30 PM"
+    time: "12:30 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "757 Gerald Crossing",
     name: "Fionna",
-    time: "3:53 AM"
+    time: "3:53 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "53 Bluestem Road",
     name: "Eada",
-    time: "6:06 AM"
+    time: "6:06 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "4 Ridge Oak Street",
     name: "Rorke",
-    time: "9:50 AM"
+    time: "9:50 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "62 Sage Court",
     name: "Monty",
-    time: "9:13 AM"
+    time: "9:13 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "9 Merrick Lane",
     name: "Lannie",
-    time: "3:42 AM"
+    time: "3:42 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "4 Arkansas Trail",
     name: "Kevina",
-    time: "12:51 AM"
+    time: "12:51 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "9 Kingsford Alley",
     name: "Adele",
-    time: "6:51 PM"
+    time: "6:51 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "79394 Transport Junction",
     name: "Christel",
-    time: "9:53 AM"
+    time: "9:53 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "164 Schlimgen Street",
     name: "Esmaria",
-    time: "6:39 PM"
+    time: "6:39 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "834 Shopko Circle",
     name: "Dorolice",
-    time: "7:56 PM"
+    time: "7:56 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "36424 Manitowish Way",
     name: "Theobald",
-    time: "10:51 AM"
+    time: "10:51 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "08 Messerschmidt Lane",
     name: "Hulda",
-    time: "1:07 PM"
+    time: "1:07 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "703 Basil Court",
     name: "Austin",
-    time: "1:10 PM"
+    time: "1:10 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "26 Carey Terrace",
     name: "Bernadene",
-    time: "5:48 PM"
+    time: "5:48 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "59877 Maywood Hill",
     name: "Lonna",
-    time: "1:41 PM"
+    time: "1:41 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "74 Farmco Center",
     name: "Doroteya",
-    time: "8:20 AM"
+    time: "8:20 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "8606 Di Loreto Way",
     name: "Saunders",
-    time: "9:04 PM"
+    time: "9:04 PM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "6 Green Ridge Place",
     name: "Grazia",
-    time: "10:10 AM"
+    time: "10:10 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }, {
     message: "8 Mitchell Hill",
     name: "Lynelle",
-    time: "3:30 AM"
+    time: "3:30 AM",
+    messages:[
+      {id:1, body:'Hello world', time:'4:08', me:true},
+      {id:2, body:'How are you', time:'4:08', me:true},
+      {id:3, body:'Thanks', time:'4:08', me:true},
+      {id:4, body:'Good night', time:'4:08', me:true},
+      {id:5, body:'Bye...!', time:'4:08', me:true},
+    ]
   }]
   ngOnInit(): void {
   }
